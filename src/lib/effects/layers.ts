1b160a2688c2ae17649110d2e89afed65b64ea77
Adam Ringhede <adamringhede@live.com>
2022-02-13 11:21:41 +0100

    Remove example code from layers.ts

--- src/lib/effects/layers.ts
+++ src/lib/effects/layers.ts
@@ -1,7 +1,5 @@
 import { mix } from '../functions';
 import { RgbaNode } from '../types';
-import { rgba, uniformSampler2d, varyingVec2 } from '../dsl';
-import { attributes } from '../common';
 
 
 /**
@@ -9,14 +7,5 @@ import { attributes } from '../common';
  * colors beneath it if transparent. 
  */
 export function layerColors(...colors: RgbaNode[]): RgbaNode {
-  return colors.reduce((combined, color) =>
-    mix(combined, color, color.a()))
+  return colors.reduce((combined, color) => mix(combined, color, color.a()))
 }
-
-
-const vUV = varyingVec2(attributes.uv)
-const baseColor = rgba('#fd3d22')
-const backgroundColor = uniformSampler2d('backgroundMap').sample(vUV)
-const foregroundColor = uniformSampler2d('detailsMap').sample(vUV)
-const finalColor = layerColors(baseColor, backgroundColor, foregroundColor)
-
