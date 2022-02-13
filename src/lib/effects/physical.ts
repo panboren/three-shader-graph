603443205eaaee60f8ebf5607db82b33e1f4ced9
Adam Ringhede <adamringhede@live.com>
2022-02-13 13:52:26 +0100

    Add support for spotlights in physical material

--- src/lib/effects/physical.ts
+++ src/lib/effects/physical.ts
@@ -1,8 +1,9 @@
 import { float, negVec3, rgb, rgba, varyingVec3 } from '../dsl';
 import { dot, normalize, saturate } from '../functions';
-import { uniformPointLights, uniformDirectionalLights, uniformHemisphereLights } from '../lights';
+import { uniformPointLights, uniformDirectionalLights, uniformHemisphereLights, uniformSpotLights } from '../lights';
 import { transformed } from '../transformed';
 import { FloatNode, RgbNode, Vec3Node } from '../types';
+import { getSpotLightInfo } from './common-material';
 import {
   BRDF_Lambert,
   Geometry,
@@ -22,6 +23,19 @@ function calculatePointLight(
 ): Vec3Node {
   const directDiffuse = uniformPointLights.sum(Vec3Node, (light) => {
     const directLight = getPointLightInfo(light, geometry);
+    const dotNL = saturate(dot(geometry.normal, directLight.direction));
+    const irradiance = dotNL.multiplyVec3(light.color);
+    return irradiance.multiply(BRDF_Lambert(material.diffuseColor));
+  });
+  return directDiffuse;
+}
+
+function calculateSpotLight(
+  geometry: Geometry,
+  material: PhysicalMaterial
+): Vec3Node {
+  const directDiffuse = uniformSpotLights.sum(Vec3Node, (light) => {
+    const directLight = getSpotLightInfo(light, geometry);
     const dotNL = saturate(dot(geometry.normal, directLight.direction));
     const irradiance = dotNL.multiplyVec3(light.color);
     return irradiance.multiply(BRDF_Lambert(material.diffuseColor));
@@ -83,9 +97,11 @@ export function standardMaterial(params: Partial<StandardMaterialParameters>) {
     viewDir: normalize(negVec3(vPos)),
   } as Geometry;
 
-  const directDiffuse = calculatePointLight(geometry, material).add(
-    calculateDirectionalLight(geometry, material)
-  );
+  const directDiffuse = [
+    calculatePointLight(geometry, material),
+    calculateDirectionalLight(geometry, material),
+    calculateSpotLight(geometry, material)
+  ].reduce((a, v) => a.add(v))
 
   const indirectDiffuse = calculateHemisphereLight(geometry, material);
 
