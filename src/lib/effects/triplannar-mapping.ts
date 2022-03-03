8cfb4a72349d7f259a586eca08cf6de84278de87
Adam Ringhede <adamringhede@live.com>
2022-03-03 19:54:40 +0100

    Fix triplannar mapping

--- src/lib/effects/triplannar-mapping.ts
+++ src/lib/effects/triplannar-mapping.ts
@@ -1,24 +1,22 @@
-import { abs, normalize } from '../functions';
-import { FloatNode, RgbaNode, Sampler2DNode, Vec3Node } from '../types';
+import { normalize, abs, dot } from "../functions"
+import { Sampler2DNode, Vec3Node, FloatNode, RgbaNode } from "../types"
+import { vec3 } from '../dsl';
 
-export function triplanarMapping(
-  texture: Sampler2DNode,
-  normal: Vec3Node,
-  position: Vec3Node,
-  scale: FloatNode
-): RgbaNode {
+
+export function triplanarMapping(texture: Sampler2DNode, normal: Vec3Node, position: Vec3Node, scale: FloatNode): RgbaNode {
   // Blending factor of triplanar mapping
-  const bf = normalize(abs(normal));
+  const bf0 = normalize(abs(normal))
+  const bf = bf0.divideScalar(dot(bf0, vec3(1, 1, 1)))
 
   // Triplanar mapping
-  const tx = position.yz().multiplyScalar(scale);
-  const ty = position.zx().multiplyScalar(scale);
-  const tz = position.xy().multiplyScalar(scale);
+  const tx = position.yz().multiplyScalar(scale)
+  const ty = position.zx().multiplyScalar(scale)
+  const tz = position.xy().multiplyScalar(scale)
 
   // Base color
-  const cx = texture.sample(tx).multiplyScalar(bf.x());
-  const cy = texture.sample(ty).multiplyScalar(bf.y());
-  const cz = texture.sample(tz).multiplyScalar(bf.z());
+  const cx = texture.sample(tx).multiplyScalar(bf.x())
+  const cy = texture.sample(ty).multiplyScalar(bf.y())
+  const cz = texture.sample(tz).multiplyScalar(bf.z())
 
-  return cx.add(cy).add(cz).rgba();
+  return cx.add(cy).add(cz).rgba()
 }
